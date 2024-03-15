46e10d561d97a24045b82500c3390f8cbf8b0cbc
bhusalnaran <161798814+bhusalnaran@users.noreply.github.com>
2024-03-15 23:22:28 +0530

    Update instaqr.py

--- instaqr.py
+++ instaqr.py
@@ -9,4 +9,4 @@ s = "https://www.instagram.com/naranbhusal127?igsh=MTlnZHdtYXRhZWh1cQ=="
 url = pyqrcode.create(s) 
   
 # Create and save the png file naming "myqr.png" 
-url.svg("myyoutube.svg", scale = 8) 
+url.svg("myinstagram.svg", scale = 8) 
