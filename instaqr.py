d9100e33e46bd63fa2165e52a14b956b4b6a2422
bhusalnaran <161798814+bhusalnaran@users.noreply.github.com>
2024-03-15 23:21:43 +0530

    Add files via upload

---
+++ instaqr.py
@@ -0,0 +1,12 @@
+
+import pyqrcode 
+from pyqrcode import QRCode 
+  
+# String which represent the QR code 
+s = "https://www.instagram.com/naranbhusal127?igsh=MTlnZHdtYXRhZWh1cQ=="
+  
+# Generate QR code 
+url = pyqrcode.create(s) 
+  
+# Create and save the png file naming "myqr.png" 
+url.svg("myyoutube.svg", scale = 8) 
